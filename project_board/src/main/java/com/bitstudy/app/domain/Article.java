28d5f82d909714abd73142dcc00d3535a1e31240
gohun <lljjll1235@gmail.com>
2022-12-19 19:13:46 +0900

    #11 - 도메인 클래스 설계
    도메인 정보 설계(Article, Comment)
    기초적인 게시글 / 댓글 표현이 가능하도록
    DB 연동을 위한 추가적인 표현은 추후 업데이트

---
+++ project_board/src/main/java/com/bitstudy/app/domain/Article.java
@@ -0,0 +1,16 @@
+package com.bitstudy.app.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String content;
+    private String hashtag;
+
+    //메타데이터
+    private LocalDateTime create_at;
+    private String create_by;
+    private LocalDateTime modify_at;
+    private String modify_by;
+}
