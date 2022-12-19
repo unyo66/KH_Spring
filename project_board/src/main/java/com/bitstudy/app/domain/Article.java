02763cfa400cdc2b8294f716ef9325e4c62193b5
gohun <lljjll1235@gmail.com>
2022-12-19 21:48:03 +0900

    임시

--- project_board/src/main/java/com/bitstudy/app/domain/Article.java
+++ project_board/src/main/java/com/bitstudy/app/domain/Article.java
@@ -1,7 +1,22 @@
 package com.bitstudy.app.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+/*  할일 : Lombok 사용하기
+*
+*   순서
+*   1) Lombok을 이용해서 클래스를 엔티티로 변경
+*   2) getter / setter, toString 등의 Lombok 어노테이션 사용
+*   3) 동등성, 동일성 비교할 수 있는 코드 넣기
+* */
+
+@Getter
+@Setter
+@Entity
 public class Article {
     private Long id;
     private String title;
