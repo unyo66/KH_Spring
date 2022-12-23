ab3e602aebf841d84aae5adb2487ef204db9bc2d
gohun <lljjll1235@gmail.com>
2022-12-23 17:05:38 +0900

    #17 - 공통코드 auditingFields 추출
    생성자, 생성일, 수정자, 수정일은 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관이 없는 메타데이터이므로 추출이 가능.
    
    @MappedSuperclass를 이용해 상속방식으로 사용가능

---
+++ project_board/src/main/java/com/bitstudy/app/domain/AuditingFields.java
@@ -0,0 +1,45 @@
+package com.bitstudy.app.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/*  할일 : Article, Comment 의 중복필드 합치기
+    1) Article 의 메타데이터들(auditing 관련 필드) 가져오기
+    2) 클래스에 @MappedSuperclass 달아주기
+    3) auditing 관련된 것들 다 가져오기
+    4)
+*
+* */
+
+@EntityListeners(AuditingEntityListener.class) //얘없으면 테스트 중 createAt 때문에 에러남(Ex04관련)
+@MappedSuperclass
+@Getter
+@ToString
+public class AuditingFields {
+    //메타데이터
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime create_at;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String create_by;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modify_at;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modify_by;
+}
